fcc0a7d52e0fe5ce44d069fb02bf36f1d740d522
kamleshhv <kamleshvansajalia@gmail.com>
2024-02-12 04:46:31 +0000

    update

--- rom.sh
+++ rom.sh
@@ -1,4 +1,3 @@
-screen
 repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 git clone https://github.com/kamleshhv/device_xiaomi_sweet.git -b matrixx device/xiaomi/sweet
